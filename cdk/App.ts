dbe9b27b293c4b5812018591d418f5c4ffbb7682
Christian Paul Dehli <cp@deh.li>
2022-01-17 09:14:15 -0500

    remove iam::role from cdk

--- cdk/App.ts
+++ cdk/App.ts
@@ -14,16 +14,7 @@ const table = new cdk.aws_dynamodb.Table(stack, "TemperatureTable", {
   },
 });
 
-const role = new cdk.aws_iam.Role(stack, "WriteToTableRole", {
-  assumedBy: new cdk.aws_iam.AccountRootPrincipal(),
-});
-
-table.grantWriteData(role);
-
 const user = new cdk.aws_iam.User(stack, "WriteToTableUser");
-user.addToPolicy(new cdk.aws_iam.PolicyStatement({
-  actions: ["sts:AssumeRole"],
-  resources: [role.roleArn],
-}));
+table.grantWriteData(user);
 
 app.synth();
