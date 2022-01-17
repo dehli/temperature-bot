ef519114d1ae44563b4af266e936526a94fc3758
Christian Paul Dehli <cp@deh.li>
2022-01-17 13:08:34 -0500

    fix pi script

--- pi/main.py
+++ pi/main.py
@@ -6,10 +6,12 @@
 ###
 
 import boto3
+import decimal
 import os
 import time
 
 # Pull out environment variables
+aws_region = os.environ.get("AWS_REGION")
 device_key = os.environ.get("TEMPERATURE_DEVICE_KEY")
 table_name = os.environ.get("TEMPERATURE_TABLE_NAME")
 file_path  = os.environ.get("TEMPERATURE_FILE_PATH")
@@ -19,23 +21,24 @@ temperature_file = open(file_path)
 temperature_text = temperature_file.read()
 temperature_file.close()
 
-time_now = time.time()
-temperature_string = temperature_text.split("\n")[1].split("t=")[1]
-temperature = float(temperature_string) / 1000
+if temperature_text:
+    time_now = time.time()
+    temperature_string = temperature_text.split("\n")[1].split("t=")[1]
+    temperature = decimal.Decimal(temperature_string) / 1000
 
-# Setup AWS Clients
-dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table(table_name)
+    # Setup AWS Clients
+    dynamodb = boto3.resource("dynamodb", region_name=aws_region)
+    table = dynamodb.Table(table_name)
 
-# Persist temperature to DynamoDB
-table.put_item(
-    Item={
-        "key": device_key,
-        "time": int(time_now),
-        "temperature": temperature
-    },
-    ConditionExpression="attribute_not_exists(#key)",
-    ExpressionAttributeNames={
-        "#key": "key"
-    }
-)
+    # Persist temperature to DynamoDB
+    table.put_item(
+        Item={
+            "key": device_key,
+            "time": int(time_now),
+            "temperature": temperature
+        },
+        ConditionExpression="attribute_not_exists(#key)",
+        ExpressionAttributeNames={
+            "#key": "key"
+        }
+    )
