7642340aba12e6efd00c06b22a11a13c23f4966b
Christian Paul Dehli <cp@deh.li>
2023-11-26 06:09:51 -0500

    add dedicated save_temp function

--- api/index.py
+++ api/index.py
@@ -2,12 +2,16 @@ import boto3
 import json
 import os
 import time
-
 from boto3.dynamodb.conditions import Key, Attr
 
-dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table(os.environ.get("TABLE_NAME"))
+# Environment variables
+api_path = os.environ.get("API_PATH")
 partition_key = os.environ.get("PARTITION_KEY")
+table_name = os.environ.get("TABLE_NAME")
+
+# AWS clients
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(table_name)
 
 script_dir = os.path.dirname(__file__)
 file_path = os.path.join(script_dir, "index.html")
@@ -15,9 +19,28 @@ fptr = open(file_path)
 html = fptr.read()
 fptr.close()
 
-def handler(event, lambda_context):
-    print(event)
+def save_temperature(event):
+    time_now = time.time()
 
+    # Persist temperature to DynamoDB
+    # table.put_item(
+    #     Item={
+    #         "key": partition_key,
+    #         "time": int(time_now),
+    #         "temperature": temperature
+    #     },
+    #     ConditionExpression="attribute_not_exists(#key)",
+    #     ExpressionAttributeNames={
+    #         "#key": "key"
+    #     }
+    # )
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(event)
+    }
+
+def generate_index_html(event):
     # Determine timestamp for 24 hours ago (in seconds)
     timestamp = int(time.time() - 24 * 60 * 60)
 
@@ -41,3 +64,9 @@ def handler(event, lambda_context):
             "content-type": "text/html"
         }
     }
+
+def handler(event, lambda_context):
+    if event["rawPath"] == api_path:
+        return save_temperature(event)
+    else:
+        return generate_index_html(event)
