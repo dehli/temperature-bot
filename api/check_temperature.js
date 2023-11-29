17ffd009a14348d4c0ab92fb74e44c4cddc7920d
cp <cp@deh.li>
2023-11-29 09:27:47 -0500

    query dynamo in checkTemp (#12)

--- api/check_temperature.js
+++ api/check_temperature.js
@@ -1,9 +1,39 @@
+const { DynamoDBClient, QueryCommand } = require("@aws-sdk/client-dynamodb");
+
+const { PARTITION_KEY, TABLE_NAME } = process.env;
+const client = new DynamoDBClient();
+
 exports.handler = async () => {
-  // TODO: query for 4 most recent temps
+  try {
+    // query for recent temperatures
+    const { Items } = await client.send(
+      new QueryCommand({
+        KeyConditionExpression: "#key = :key",
+        ExpressionAttributeNames: {
+          "#key": "key",
+        },
+        ExpressionAttributeValues: {
+          ":key": { S: PARTITION_KEY },
+        },
+        Limit: 3,
+        ScanIndexForward: false,
+        TableName: TABLE_NAME,
+      }),
+    );
 
-  // TODO: check that most recent is within 10 minutes
+    // check if temperature reading is too old
+    const latestAgeInSeconds = Date.now() / 1000 - Items[0].time.N;
+    if (latestAgeInSeconds >= 600) {
+      throw new Error(`Temperature not recent (${latestAgeInSeconds} seconds)`);
+    }
 
-  // TODO: check that the lowest of the temps is greater than limit
-
-  return { success: true };
+    // check if temperature reading is too cold
+    const lowestTemp = Math.min(...Items.map((i) => +i.temperature.N));
+    if (lowestTemp <= 4) {
+      throw new Error(`Temperature too low (${lowestTemp})`);
+    }
+  } catch (e) {
+    // TODO: send email with error message
+    console.log(e.message);
+  }
 };
