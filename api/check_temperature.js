df1a79a0450d613a826515f35a01718c8d1ef134
Christian Paul Dehli <cp@deh.li>
2023-11-29 08:31:37 -0500

    add checkTemp lambda

---
+++ api/check_temperature.js
@@ -0,0 +1,9 @@
+exports.handler = async () => {
+  // TODO: query for 4 most recent temps
+
+  // TODO: check that most recent is within 10 minutes
+
+  // TODO: check that the lowest of the temps is greater than limit
+
+  return { success: true };
+};
