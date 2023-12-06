307e8163caa75041349d6873b1789903971e51d6
Christian Paul Dehli <cp@deh.li>
2023-12-06 07:22:52 -0500

    make pi script more robust

--- pi/main.py
+++ pi/main.py
@@ -12,16 +12,48 @@ import os
 
 # Pull out environment variables
 base_url = os.environ.get("API_URL")
-file_path  = os.environ.get("TEMPERATURE_FILE_PATH")
+file_path = os.environ.get("TEMPERATURE_FILE_PATH")
+error_log_file = "error_log.txt"
+
+# Error handling
+def log_error(error_message):
+    with open(error_log_file, "a") as file:
+        file.write(error_message + "\n")
+
+    # Check if the number of lines in error_log_file is a multiple of 5
+    with open(error_log_file, "r") as file:
+        line_count = sum(1 for line in file)
+        if line_count % 5 == 0:
+            restart_machine()
+
+def reset_error_log():
+    with open(error_log_file, "w"):
+        pass
+
+def restart_machine():
+    os.system("shutdown -r now")
 
 # Read contents of temperature recordings
-temperature_file = open(file_path)
-temperature_text = temperature_file.read()
-temperature_file.close()
+try:
+    with open(file_path, "r") as temperature_file:
+        temperature_text = temperature_file.read()
+except FileNotFoundError:
+    log_error("Error: Temperature file not found.")
+    exit()
 
 if temperature_text:
-    temperature_string = re.match("[\s\S]*t=(\d+)", temperature_text).group(1)
-    temperature = decimal.Decimal(temperature_string) / 1000
+    temperature_match = re.match("[\s\S]*t=(\d+)", temperature_text)
+    if temperature_match:
+        temperature_string = temperature_match.group(1)
+        temperature = decimal.Decimal(temperature_string) / 1000
 
-    api_url = '{}?t={}'.format(base_url, temperature)
-    requests.get(api_url)
+        api_url = '{}?t={}'.format(base_url, temperature)
+        try:
+            requests.get(api_url)
+            reset_error_log()
+        except Exception as exception:
+            log_error(f"Error sending data to API: {str(exception)}")
+    else:
+        log_error("Error: Temperature data not found in the file.")
+else:
+    log_error("Error: Empty temperature file.")
